fb0d10d01483a7c39050f211ddb20029bfe00e4e
jun68ykt <jun68ykt@teratail>
2019-07-31 09:21:54 +0900

    this.state.typesの内容に応じた数のMyStateがrenderされるように修正

--- src/App.js
+++ src/App.js
@@ -8,32 +8,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      aaa: 0,
-      bbb: 0,
+      types: [
+        { name:'aaa', count:0, roletype: 'りんご' },
+        { name:'bbb', count:0, roletype: 'みかん' },
+        { name:'ccc', count:0, roletype: 'バナナ' }
+      ],
       mess: ""
     };
   }
 
   handleClickInc(e) {
-    /*alert(e.timeStamp+"に"+e.target.name+"を増やしました。");*/
-    /*this.setState({[e.target.name]:this.state[e.target.name]+1,mess:"aaaを1増やしました。"});*/
-
-    const { name } = e.target;
-    this.setState((prevState, props) => ({
-      [name]: prevState[name] + 1,
-      mess: "aaaを1増やしました。"
-    }));
+    const nextTypes = this.state.types.map(type => ({
+      ...type,
+      count: type.count + (type.name === e.target.name)
+    }))
+    this.setState({
+      types: nextTypes,
+      mess: `${e.target.name}を1増やしました。`
+    });
   }
 
   handleClickDec(e) {
-    /*alert(e.timeStamp+"に"+e.target.name+"減らしました。");*/
-    /*this.setState({[e.target.name]:prevState[e.target.name]-1,mess:"aaaを1減らしました。"});*/
-
-    const { name } = e.target;
-    this.setState((prevState, props) => ({
-      [name]: prevState[name] - 1,
-      mess: "aaaを1減らしました。"
-    }));
+    const nextTypes = this.state.types.map(type => ({
+      ...type,
+      count: type.count - (type.name === e.target.name)
+    }))
+    this.setState({
+      types: nextTypes,
+      mess: `${e.target.name}を1減らしました。`
+    });
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -44,34 +47,32 @@ class App extends React.Component {
        localStorage.setItem('bbb',this.state.bbb);
     */
 
-    localStorage.setItem("aaa", this.state.aaa);
-    localStorage.setItem("bbb", this.state.bbb);
+    this.state.types.forEach(type => {
+      localStorage.setItem(type.name, type.count);
+    })
   }
 
   render() {
     return (
       <div>
-        <p>{localStorage.getItem("aaa")}</p>
-        <p>{localStorage.getItem("bbb")}</p>
+        {this.state.types.map(type => (<p>{localStorage.getItem(type.name)}</p>))}
         <MessBox mess={this.state.mess} />
-        <h1>aaa</h1>
-        <MyState
-          nor={this.state.aaa}
-          name="aaa"
-          inc_mess="+1"
-          dec_mess="-1"
-          onClickInc={e => this.handleClickInc(e)}
-          onClickDec={e => this.handleClickDec(e)}
-        />
-        <h1>bbb</h1>
-        <MyState
-          nor={this.state.bbb}
-          name="bbb"
-          inc_mess="+1"
-          dec_mess="-1"
-          onClickInc={e => this.handleClickInc(e)}
-          onClickDec={e => this.handleClickDec(e)}
-        />
+        {
+          this.state.types.map(type => (
+            <div key={type.name}>
+              <h1>{type.name}</h1>
+              <MyState
+                roletype={type.roletype}
+                nor={type.count}
+                name={type.name}
+                inc_mess="+1"
+                dec_mess="-1"
+                onClickInc={e => this.handleClickInc(e)}
+                onClickDec={e => this.handleClickDec(e)}
+              />
+            </div>
+          ))
+        }
       </div>
     );
   }
